42c454ba6872ae18be9efdfdca899dcd96f33de9
ariamaranai <106607480+ariamaranai@users.noreply.github.com>
2025-05-04 01:07:12 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -2,16 +2,32 @@
 Object.prototype.hasOwnProperty.call = (a, b) => {
   switch (b) {
     case "alt":
+    case "aria-activedescendant":
+    case "aria-autocomplete":
+    case "aria-current":
     case "aria-busy":
     case "aria-controls":
+    case "aria-describedby":
     case "aria-disabled":
     case "aria-expanded":
     case "aria-haspopup":
     case "aria-hidden":
     case "aria-label":
+    case "aria-labelledby":
     case "aria-live":
+    case "aria-multiline":
+    case "aria-owns":
     case "aria-pressed":
     case "aria-required":
+    case "ariaActiveDescendantID":
+    case "ariaAutoComplete":
+    case "ariaExpanded":
+    case "ariaHasPopup":
+    case "ariaLabel":
+    case "ariaOwneeID":
+    case "ariaProps":
+    case "backgroundColor":
+    case "color":
     case "data-test-id":
     case "data-test-image-signature":
     case "data-test-pin-id":
@@ -20,7 +36,9 @@ Object.prototype.hasOwnProperty.call = (a, b) => {
     // case "direction":
     case "fetchPriority":
     case "fetchpriority":
+    case "loop":
     case "onError":
+    case "opacity":
     case "onTouchEnd":
     case "onTouchStart":
       return 0;
