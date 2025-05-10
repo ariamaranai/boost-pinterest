1afeb30f838e68475da1f6390eda3fd024fa8c78
ariamaranai <106607480+ariamaranai@users.noreply.github.com>
2025-05-10 10:31:00 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -1,10 +1,14 @@
-// var kkk = {};
 {
+  // var z = {};
   let has = (a, b) => {
     switch (b) {
       case "alt":
+      case "animation":
+      case "animationDelay":
+      case "animationName":
       case "aria-activedescendant":
       case "aria-autocomplete":
+      case "aria-checked":
       case "aria-current":
       case "aria-busy":
       case "aria-controls":
@@ -27,24 +31,55 @@
       case "ariaLabel":
       case "ariaOwneeID":
       case "ariaProps":
+      case "autoCapitalize":
       case "backgroundColor":
+      case "borderColor":
+      case "borderRadius":
+      case "borderRight":
+      case "borderStyle":
+      case "borderTop":
+      case "boxShadow":
       case "color":
       case "data-test-id":
       case "data-test-image-signature":
       case "data-test-pin-id":
       case "data-test-pin-slot-index":
       case "dataTestId":
+      case "disabled":
       // case "direction":
       case "fetchPriority":
       case "fetchpriority":
+      case "isDragWithinBounds":
       case "loop":
+      case "onAnimationIteration":
+      case "onAnimationEnd":
+      case "onAnimationStart":
+      case "onContextMenu":
+      case "onDoubleClick":
+      case "onDragEnd":
+      case "onDragEnter":
+      case "onDragLeave":
+      case "onDragOver":
+      case "onDragStart":
+      case "onDrop":
       case "onError":
+      case "onInvalid":
+      case "onTouchCancel":
       case "onTouchEnd":
+      case "onTouchMove":
       case "onTouchStart":
+      case "onVolumeChange":
       case "opacity":
+      case "tabIndex":
+      case "textDirectionality":
+      case "theme":
+      case "tiltX":
+      case "tiltY":
+      case "title":
+      case "twist":
         return 0;
       default:
-        // typeof b == "string" && b.length > 2 && (kkk[b] ? ++kkk[b] : kkk[b] = 1);
+        // typeof b == "string" && b.length > 2 && (z[b] ? ++z[b] : z[b] = 1);
         return b in a;
     }
   }
